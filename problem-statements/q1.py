20e8149e4473877c2b3eefc88d7b75ddaa41c0c9
Lolugu Dheeraj <126643540+dheeraj172@users.noreply.github.com>
2026-02-16 10:55:33 +0530

    q1.py

--- problem-statements/q1.py
+++ problem-statements/q1.py
@@ -1,1 +1,13 @@
+#team name : default errors
 
+def calculate_total_bill(amount: float, tip_percent: int )->float:
+  amount = float(amount)
+  tip_percent = float(tip_percent)
+  
+  if not (0 <=amount<=10000):
+   raise ValueError("amount must be b/w 0 and 10000")
+  if not (0<=tip_percent<=100):
+   raise ValueError("amount must be b/w 0 and 100")  
+    
+  total_bill = amount + (amount * tip_percent)/100
+  return round(total_bill,2)
